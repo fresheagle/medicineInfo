16b6f8776cd89ab32a2911b5dceac3080bd68eb0
fresheagle <zm289385250@126.com>
2018-11-19 23:29:52 +0800

    add by zhangmin

--- medicineInfo_rest/src/main/java/com/med/info/rest/DiseaseController.java
+++ medicineInfo_rest/src/main/java/com/med/info/rest/DiseaseController.java
@@ -1,5 +1,19 @@
 package com.med.info.rest;
 
-public class DiseaseController {
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Title: 疾病参数配置管理 
+ * Description: 对疾病相关参数的增删改查
+ * 
+ * @author DELETE
+ * 
+ */
+@RestController
+@RequestMapping("/disease")
+public class DiseaseController {
+	private static final Logger log = Logger.getLogger(DiseaseController.class);
+	
 }
--- medicineInfo_rest/src/main/java/com/med/info/rest/MedicalController.java
+++ medicineInfo_rest/src/main/java/com/med/info/rest/MedicalController.java
@@ -1,5 +1,19 @@
 package com.med.info.rest;
 
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Title: 药品信息的参数管理 
+ * Description: 药品信息数据的增删改查
+ * 
+ * @author DELETE
+ * 
+ */
+@RestController
+@RequestMapping("/medical")
 public class MedicalController {
+	private static final Logger log = Logger.getLogger(MedicalController.class);
 
 }

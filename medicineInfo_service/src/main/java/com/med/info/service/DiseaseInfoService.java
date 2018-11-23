0ca3220299b784044218e790416d62e4ec0e5421
DELETE <289385250@qq.com>
2018-11-23 09:41:32 +0800

    修改拼写错误

--- medicineInfo_service/src/main/java/com/med/info/service/DiseaseInfoService.java
+++ medicineInfo_service/src/main/java/com/med/info/service/DiseaseInfoService.java
@@ -24,131 +24,131 @@ public interface DiseaseInfoService {
 	 * @param diseaseDTO 疾病基础信息
 	 * @return 疾病信息id
 	 */
-	public Integer addDiseaseInfo(DiseaseDTO diseaseDTO);
+	Integer addDiseaseInfo(DiseaseDTO diseaseDTO);
 	
 	/**
 	 * 添加中医疾病信息
 	 * @param miss_chinese_diseaseWithBLOBs 包含父类信息参数的中医疾病信息
 	 * @return 中医疾病信息id
 	 */
-	public Integer addChineseDiseaseBlogInfo(Miss_chinese_diseaseWithBLOBs miss_chinese_diseaseWithBLOBs);
+	Integer addChineseDiseaseBlogInfo(Miss_chinese_diseaseWithBLOBs miss_chinese_diseaseWithBLOBs);
 	
 	/**
 	 * 添加西医疾病信息
 	 * @param miss_western_diseaseWithBLOBs 包含父类信息参数的西医疾病信息
 	 * @return 西医疾病信息id
 	 */
-	public Integer addWestDiseaseBlogInfo(Miss_western_diseaseWithBLOBs miss_western_diseaseWithBLOBs);
+	Integer addWestDiseaseBlogInfo(Miss_western_diseaseWithBLOBs miss_western_diseaseWithBLOBs);
 	
 	/**
 	 * 添加中西医结合疾病信息
 	 * @param miss_combination_diseaseWithBLOBs 包含父类信息参数的中西医结合疾病信息
 	 * @return 中西医结合疾病信息id
 	 */
-	public Integer addCombinationDiseaseBlogInfo(Miss_combination_diseaseWithBLOBs miss_combination_diseaseWithBLOBs);
+	Integer addCombinationDiseaseBlogInfo(Miss_combination_diseaseWithBLOBs miss_combination_diseaseWithBLOBs);
 	
 	/**
 	 * 查询疾病基础信息
 	 * @param diseaseId 疾病基础信息id
 	 * @return 疾病基础信息内容
 	 */
-    public DiseaseDTO queryDiseaseInfo(Integer diseaseId);
+	DiseaseDTO queryDiseaseInfo(Integer diseaseId);
     
     /**
 	 * 查询疾病id与名称
 	 * @param
 	 * @return 疾病基础信息的id和名称列表
 	 */
-    public List<Map<Integer,String>> queryDiseaseInfoName();
+    List<Map<Integer,String>> queryDiseaseInfoName();
 	
     /**
 	 * 查询疾病相关科室部门信息
 	 * @param
 	 * @return 疾病相关科室部门信息
 	 */
-    public List<DiseaseDepartmentMapDTO> queryDepartmentInfo();
+	List<DiseaseDepartmentMapDTO> queryDepartmentInfo();
     
     /**
      * 查询疾病相关症状信息
      * @param
      * @return 疾病相关症状信息
      */
-    public List<DiseaseSymptomMapDTO> querySymptomInfo();
+	List<DiseaseSymptomMapDTO> querySymptomInfo();
     
     /**
      * 查询中医疾病相关信息
      * @param chinese_diseaseId 中医疾病相关信息id
      * @return 中医疾病信息
      */
-	public Miss_chinese_diseaseWithBLOBs queryChineseDiseaseBlogInfo(Integer chinese_diseaseId);
+	Miss_chinese_diseaseWithBLOBs queryChineseDiseaseBlogInfo(Integer chinese_diseaseId);
 	
 	/**
 	 * 查询西医疾病相关信息
 	 * @param western_diseaseId 西医疾病相关信息id
 	 * @return 西医疾病信息
 	 */
-	public Miss_western_diseaseWithBLOBs queryWestDiseaseBlogInfo(Integer western_diseaseId);
+	Miss_western_diseaseWithBLOBs queryWestDiseaseBlogInfo(Integer western_diseaseId);
 	
 	/**
 	 * 查询中西医结合疾病相关信息 
 	 * @param combination_diseaseId 中西医结合疾病相关信息id
 	 * @return 中西医结合疾病相关信息
 	 */
-	public Miss_combination_diseaseWithBLOBs queryCombinationDiseaseBlogInfo(Integer combination_diseaseId);
+	Miss_combination_diseaseWithBLOBs queryCombinationDiseaseBlogInfo(Integer combination_diseaseId);
 	
 	/**
 	 * 修改疾病基础信息
 	 * @param diseaseDTO 疾病基础信息
 	 * @return 疾病信息id
 	 */
-	public Integer updateDiseaseInfo(DiseaseDTO diseaseDTO);
+	Integer updateDiseaseInfo(DiseaseDTO diseaseDTO);
 
 	/**
 	 * 修改中医疾病信息
 	 * @param miss_chinese_diseaseWithBLOBs 包含父类信息参数的中医疾病信息
 	 * @return 中医疾病信息id
 	 */
-	public Integer updateChineseDiseaseBlogInfo(Miss_chinese_diseaseWithBLOBs miss_chinese_diseaseWithBLOBs);
+	Integer updateChineseDiseaseBlogInfo(Miss_chinese_diseaseWithBLOBs miss_chinese_diseaseWithBLOBs);
 	
 	/**
 	 * 修改西医疾病信息
 	 * @param miss_western_diseaseWithBLOBs 包含父类信息参数的西医疾病信息
 	 * @return 西医疾病信息id
 	 */
-	public Integer updateWestDiseaseBlogInfo(Miss_western_diseaseWithBLOBs miss_western_diseaseWithBLOBs);
+	Integer updateWestDiseaseBlogInfo(Miss_western_diseaseWithBLOBs miss_western_diseaseWithBLOBs);
 	
 	/**
 	 * 修改中西医结合疾病信息
 	 * @param miss_combination_diseaseWithBLOBs 包含父类信息参数的中西医结合疾病信息
 	 * @return 中西医结合疾病信息id
 	 */
-	public abstract Integer updateCombinationDiseaseBlogInfo(Miss_combination_diseaseWithBLOBs miss_combination_diseaseWithBLOBs);
+	Integer updateCombinationDiseaseBlogInfo(Miss_combination_diseaseWithBLOBs miss_combination_diseaseWithBLOBs);
 	
 	/**
 	 * 删除疾病基础信息
 	 * @param diseaseId 疾病基础信息id
 	 * @return 疾病信息id
 	 */
-	public Integer deleteDiseaseInfo(Integer diseaseId);
+	Integer deleteDiseaseInfo(Integer diseaseId);
 	
 	/**
 	 * 删除中医疾病信息
 	 * @param chinese_diseaseId 包含父类信息参数的中医疾病信息id
 	 * @return 中医疾病信息id
 	 */
-	public Integer deleteChineseDiseaseBlogInfo(Integer chinese_diseaseId);
+	Integer deleteChineseDiseaseBlogInfo(Integer chinese_diseaseId);
 	
 	/**
 	 * 删除西医疾病信息
 	 * @param western_diseaseId 包含父类信息参数的西医疾病信息id
 	 * @return 西医疾病信息id
 	 */
-	public Integer deleteWestDiseaseBlogInfo(Integer western_diseaseId);
+	Integer deleteWestDiseaseBlogInfo(Integer western_diseaseId);
 	
 	/**
 	 * 删除中西医结合疾病信息
 	 * @param combination_diseaseId 包含父类信息参数的中西医结合疾病信息id
 	 * @return 中西医结合疾病信息id
 	 */
-	public Integer deleteCombinationDiseaseBlogInfo(Integer combination_diseaseId);
+	Integer deleteCombinationDiseaseBlogInfo(Integer combination_diseaseId);
 }

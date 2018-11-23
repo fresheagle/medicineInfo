9842d8fdc62475848a6fd5f689bf1e95d477562c
DELETE <289385250@qq.com>
2018-11-23 09:41:59 +0800

    修改拼写错误

--- medicineInfo_rest/src/main/java/com/med/info/rest/ChineseDiseaseController.java
+++ medicineInfo_rest/src/main/java/com/med/info/rest/ChineseDiseaseController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.med.info.domain.Miss_chinese_diseaseWithBLOBs;
 import com.med.info.response.Response;
-import com.med.info.service.DiseasInfoService;
+import com.med.info.service.DiseaseInfoService;
 
 /**
  * Title: 中医疾病参数配置管理 
@@ -24,29 +24,29 @@ public class ChineseDiseaseController {
 	private static final Logger log = Logger.getLogger(DiseaseController.class);
 	
 	@Autowired
-	DiseasInfoService diseasInfoService;
+	DiseaseInfoService diseaseInfoService;
 	
 	@RequestMapping(method = RequestMethod.POST)
 	public Response addChineseDisease(@RequestBody Miss_chinese_diseaseWithBLOBs miss_chinese_diseaseWithBLOBs) {
-		diseasInfoService.addChineseDiseaseBlogInfo(miss_chinese_diseaseWithBLOBs);
+		diseaseInfoService.addChineseDiseaseBlogInfo(miss_chinese_diseaseWithBLOBs);
 		return null;
 	}
 	
 	@RequestMapping(method = RequestMethod.PUT)
 	public Response updateChineseDisease(@RequestBody Miss_chinese_diseaseWithBLOBs miss_chinese_diseaseWithBLOBs) {
-		diseasInfoService.updateChineseDiseaseBlogInfo(miss_chinese_diseaseWithBLOBs);
+		diseaseInfoService.updateChineseDiseaseBlogInfo(miss_chinese_diseaseWithBLOBs);
 		return null;
 	}
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public Response queryChineseDisease(@RequestBody Integer chinese_diseaseId) {
-		diseasInfoService.queryChineseDiseaseBlogInfo(chinese_diseaseId);
+		diseaseInfoService.queryChineseDiseaseBlogInfo(chinese_diseaseId);
 		return null;
 	}
 	
 	@RequestMapping(method = RequestMethod.DELETE)
 	public Response deleteChineseDisease(@RequestBody Integer chinese_diseaseId) {
-		diseasInfoService.deleteChineseDiseaseBlogInfo(chinese_diseaseId);
+		diseaseInfoService.deleteChineseDiseaseBlogInfo(chinese_diseaseId);
 		return null;
 	}
 }

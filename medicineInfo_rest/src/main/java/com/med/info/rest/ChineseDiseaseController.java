f2a81d8c147ed3c2ea5408644c1bde0d22a96d6d
DELETE <289385250@qq.com>
2018-11-23 18:04:42 +0800

    更新rest层的接口信息和注释

--- medicineInfo_rest/src/main/java/com/med/info/rest/ChineseDiseaseController.java
+++ medicineInfo_rest/src/main/java/com/med/info/rest/ChineseDiseaseController.java
@@ -25,28 +25,59 @@ public class ChineseDiseaseController {
 	
 	@Autowired
 	DiseaseInfoService diseaseInfoService;
-	
+
+	/**
+	 * 添加中医疾病信息
+	 * @param miss_chinese_diseaseWithBLOBs 包含父类信息参数的中医疾病信息
+	 * @return 中医疾病信息id
+	 */
 	@RequestMapping(method = RequestMethod.POST)
 	public Response addChineseDisease(@RequestBody Miss_chinese_diseaseWithBLOBs miss_chinese_diseaseWithBLOBs) {
-		diseaseInfoService.addChineseDiseaseBlogInfo(miss_chinese_diseaseWithBLOBs);
-		return null;
+		try{
+			return new Response().success(diseaseInfoService.addChineseDiseaseBlogInfo(miss_chinese_diseaseWithBLOBs));
+		}catch (Exception e){
+			return new Response().failure();
+		}
 	}
-	
+
+	/**
+	 * 修改中医疾病信息
+	 * @param miss_chinese_diseaseWithBLOBs 包含父类信息参数的中医疾病信息
+	 * @return 中医疾病信息id
+	 */
 	@RequestMapping(method = RequestMethod.PUT)
 	public Response updateChineseDisease(@RequestBody Miss_chinese_diseaseWithBLOBs miss_chinese_diseaseWithBLOBs) {
-		diseaseInfoService.updateChineseDiseaseBlogInfo(miss_chinese_diseaseWithBLOBs);
-		return null;
+		try{
+			return new Response().success(diseaseInfoService.updateChineseDiseaseBlogInfo(miss_chinese_diseaseWithBLOBs));
+		}catch (Exception e){
+			return new Response().failure();
+		}
 	}
-	
+
+	/**
+	 * 查询中医疾病相关信息
+	 * @param chinese_diseaseId 中医疾病相关信息id
+	 * @return 中医疾病信息
+	 */
 	@RequestMapping(method = RequestMethod.GET)
 	public Response queryChineseDisease(@RequestBody Integer chinese_diseaseId) {
-		diseaseInfoService.queryChineseDiseaseBlogInfo(chinese_diseaseId);
-		return null;
+		try{
+			return new Response().success(diseaseInfoService.queryChineseDiseaseBlogInfo(chinese_diseaseId));
+		}catch (Exception e){
+			return new Response().failure();
+		}
 	}
-	
+	/**
+	 * 删除中医疾病信息
+	 * @param chinese_diseaseId 包含父类信息参数的中医疾病信息id
+	 * @return 中医疾病信息id
+	 */
 	@RequestMapping(method = RequestMethod.DELETE)
 	public Response deleteChineseDisease(@RequestBody Integer chinese_diseaseId) {
-		diseaseInfoService.deleteChineseDiseaseBlogInfo(chinese_diseaseId);
-		return null;
+		try{
+			return new Response().success(diseaseInfoService.deleteChineseDiseaseBlogInfo(chinese_diseaseId));
+		}catch (Exception e){
+			return new Response().failure();
+		}
 	}
 }

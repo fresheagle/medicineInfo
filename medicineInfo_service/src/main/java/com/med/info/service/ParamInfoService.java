f0302d6cd3d837a07893aa1994ca5d06d31c6f82
DELETE <289385250@qq.com>
2018-11-23 12:24:44 +0800

    新增查询参数列表接口服务

---
+++ medicineInfo_service/src/main/java/com/med/info/service/ParamInfoService.java
@@ -0,0 +1,44 @@
+package com.med.info.service;
+
+import com.med.info.service.dto.DepartmentMapDTO;
+import com.med.info.service.dto.SymptomMapDTO;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Title: 列表参数查询服务
+ * Description: 列表参数查询服务
+ * @author DELETE
+ *
+ */
+public interface ParamInfoService {
+    /**
+     * 查询疾病id与名称
+     * @param
+     * @return 疾病基础信息的id和名称列表
+     */
+    List<Map<Integer,String>> queryDiseaseInfoName();
+
+    /**
+     * 查询医疗机构id与名称
+     * @param
+     * @return 医疗机构信息的id和名称列表
+     */
+    List<Map<Integer,String>> queryInstitutionInfoName();
+
+    /**
+     * 查询疾病相关科室部门信息
+     * @param
+     * @return 疾病相关科室部门信息
+     */
+    List<DepartmentMapDTO> queryDepartmentInfo();
+
+    /**
+     * 查询疾病相关症状信息
+     * @param
+     * @return 疾病相关症状信息
+     */
+    List<SymptomMapDTO> querySymptomInfo();
+
+}

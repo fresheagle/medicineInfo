0105fa239a76e5275a01a7164d60daed3a614615
DELETE <289385250@qq.com>
2018-11-23 12:25:43 +0800

    新增机构信息传输服务实现类

---
+++ medicineInfo_service/src/main/java/com/med/info/service/impl/InstitutionInfoServiceImpl.java
@@ -0,0 +1,28 @@
+package com.med.info.service.impl;
+
+import com.med.info.service.InstitutionInfoService;
+import com.med.info.service.dto.InstitutionInfoDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InstitutionInfoServiceImpl implements InstitutionInfoService {
+    @Override
+    public Integer addInstitutionInfo(InstitutionInfoDTO institutionInfoDTO) {
+        return null;
+    }
+
+    @Override
+    public Integer updateInstitutionInfo(InstitutionInfoDTO institutionInfoDTO) {
+        return null;
+    }
+
+    @Override
+    public InstitutionInfoDTO queryInstitutionInfo(Integer institutionId) {
+        return null;
+    }
+
+    @Override
+    public InstitutionInfoDTO deleteInstitutionInfo(Integer institutionId) {
+        return null;
+    }
+}

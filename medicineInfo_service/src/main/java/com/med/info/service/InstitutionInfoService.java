61ecf4d717a5095df0c18c41d041607d38f5c0fe
DELETE <289385250@qq.com>
2018-11-23 12:25:31 +0800

    新增机构信息传输服务

--- medicineInfo_service/src/main/java/com/med/info/service/InstitutionInfoService.java
+++ medicineInfo_service/src/main/java/com/med/info/service/InstitutionInfoService.java
@@ -1,5 +1,39 @@
 package com.med.info.service;
 
+import com.med.info.service.dto.InstitutionInfoDTO;
+
+/**
+ * Title: 医疗机构信息维护
+ * Description: 医疗机构信息维护
+ * @author DELETE
+ *
+ */
 public interface InstitutionInfoService {
 
+    /**
+     * 添加医疗机构信息
+     * @param institutionInfoDTO 医疗机构信息
+     * @return 医疗机构id
+     */
+    Integer addInstitutionInfo(InstitutionInfoDTO institutionInfoDTO);
+
+    /**
+     * 修改医疗机构信息
+     * @param institutionInfoDTO 医疗机构信息
+     * @return 医疗机构id
+     */
+    Integer updateInstitutionInfo(InstitutionInfoDTO institutionInfoDTO);
+
+    /**
+     * 查询医疗机构信息
+     * @param institutionId 医疗机构id
+     * @return
+     */
+    InstitutionInfoDTO queryInstitutionInfo(Integer institutionId);
+    /**
+     * 删除医疗机构信息
+     * @param institutionId 医疗机构id
+     * @return
+     */
+    InstitutionInfoDTO deleteInstitutionInfo(Integer institutionId);
 }

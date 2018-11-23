2795ca1377d61c4e5caaf8c28be4892cac0434b8
DELETE <289385250@qq.com>
2018-11-23 09:36:19 +0800

    修改拼写错误

--- medicineInfo_service/src/main/java/com/med/info/service/impl/DiseaseInfoServiceImp.java
+++ medicineInfo_service/src/main/java/com/med/info/service/impl/DiseaseInfoServiceImp.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @Service
-public class DiseaseInfoServiceImp extends DiseaseInfoService {
+public class DiseaseInfoServiceImp implements DiseaseInfoService {
 
     @Override
     public Integer addDiseaseInfo(DiseaseDTO diseaseDTO) {

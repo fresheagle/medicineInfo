1412eb65800fccd482e31481bd48ec813043d838
DELETE <289385250@qq.com>
2018-11-23 09:41:43 +0800

    修改拼写错误

--- medicineInfo_rest/src/main/java/com/med/info/rest/WestDiseaseController.java
+++ medicineInfo_rest/src/main/java/com/med/info/rest/WestDiseaseController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.med.info.domain.Miss_western_diseaseWithBLOBs;
 import com.med.info.response.Response;
-import com.med.info.service.DiseasInfoService;
+import com.med.info.service.DiseaseInfoService;
 
 /**
  * Title: 西医疾病参数配置管理 
@@ -25,29 +25,29 @@ public class WestDiseaseController {
 	private static final Logger log = Logger.getLogger(WestDiseaseController.class);
 		
 	@Autowired
-	DiseasInfoService diseasInfoService;
+	DiseaseInfoService diseaseInfoService;
 		
 	@RequestMapping(method = RequestMethod.POST)
 	public Response addWestDiseaseBlogInfo(@RequestBody Miss_western_diseaseWithBLOBs miss_western_diseaseWithBLOBs) {
-		diseasInfoService.addWestDiseaseBlogInfo(miss_western_diseaseWithBLOBs);
+		diseaseInfoService.addWestDiseaseBlogInfo(miss_western_diseaseWithBLOBs);
 		return null;
 	}
 		
 	@RequestMapping(method = RequestMethod.PUT)
 	public Response updateWestDiseaseBlogInfo(@RequestBody Miss_western_diseaseWithBLOBs miss_western_diseaseWithBLOBs) {
-		diseasInfoService.updateWestDiseaseBlogInfo(miss_western_diseaseWithBLOBs);
+		diseaseInfoService.updateWestDiseaseBlogInfo(miss_western_diseaseWithBLOBs);
 		return null;
 	}
 		
 	@RequestMapping(method = RequestMethod.GET)
 	public Response queryWestDiseaseBlogInfo(@RequestBody Integer western_diseaseId) {
-		diseasInfoService.queryWestDiseaseBlogInfo(western_diseaseId);
+		diseaseInfoService.queryWestDiseaseBlogInfo(western_diseaseId);
 		return null;
 	}
 		
 	@RequestMapping(method = RequestMethod.DELETE)
 	public Response deleteWestDiseaseBlogInfo(@RequestBody Integer western_diseaseId) {
-		diseasInfoService.deleteWestDiseaseBlogInfo(western_diseaseId);
+		diseaseInfoService.deleteWestDiseaseBlogInfo(western_diseaseId);
 		return null;	
 	}
 }

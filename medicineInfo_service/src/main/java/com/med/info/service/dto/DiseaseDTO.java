cc85b300b13aefc43fb13d660dd7ca58f5c3066a
DELETE <289385250@qq.com>
2018-11-23 12:27:35 +0800

    修改疾病基础信息传输DTO

--- medicineInfo_service/src/main/java/com/med/info/service/dto/DiseaseDTO.java
+++ medicineInfo_service/src/main/java/com/med/info/service/dto/DiseaseDTO.java
@@ -5,13 +5,17 @@ import java.util.List;
 
 import com.med.info.domain.Miss_disease;
 
+/**
+ * 疾病基础信息传输实体类
+ * @author zhangmin
+ */
 public class DiseaseDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
 	private Miss_disease miss_disease;
-	private List<DiseaseDepartmentMapDTO> disease_department_mapping_list;
-	private List<DiseaseSymptomMapDTO> disease_symptom_mapping_list;
+	private List<DepartmentMapDTO> disease_department_mapping_list;
+	private List<SymptomMapDTO> disease_symptom_mapping_list;
 	
 	public Miss_disease getMiss_disease() {
 		return miss_disease;
@@ -19,16 +23,16 @@ public class DiseaseDTO implements Serializable{
 	public void setMiss_disease(Miss_disease miss_disease) {
 		this.miss_disease = miss_disease;
 	}
-	public List<DiseaseDepartmentMapDTO> getDisease_department_mapping_list() {
+	public List<DepartmentMapDTO> getDisease_department_mapping_list() {
 		return disease_department_mapping_list;
 	}
-	public void setDisease_department_mapping_list(List<DiseaseDepartmentMapDTO> disease_department_mapping_list) {
+	public void setDisease_department_mapping_list(List<DepartmentMapDTO> disease_department_mapping_list) {
 		this.disease_department_mapping_list = disease_department_mapping_list;
 	}
-	public List<DiseaseSymptomMapDTO> getDisease_symptom_mapping_list() {
+	public List<SymptomMapDTO> getDisease_symptom_mapping_list() {
 		return disease_symptom_mapping_list;
 	}
-	public void setDisease_symptom_mapping_list(List<DiseaseSymptomMapDTO> disease_symptom_mapping_list) {
+	public void setDisease_symptom_mapping_list(List<SymptomMapDTO> disease_symptom_mapping_list) {
 		this.disease_symptom_mapping_list = disease_symptom_mapping_list;
 	}
 }

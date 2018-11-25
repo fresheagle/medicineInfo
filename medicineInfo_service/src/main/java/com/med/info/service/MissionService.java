02aaeb166764e2697d7e1ea57551116022dac3c2
fresheagle <289385250@126.com>
2018-11-25 20:09:49 +0800

    add log

--- medicineInfo_service/src/main/java/com/med/info/service/MissionService.java
+++ medicineInfo_service/src/main/java/com/med/info/service/MissionService.java
@@ -2,6 +2,11 @@ package com.med.info.service;
 
 import com.med.info.service.dto.OperateDTO;
 
+/**
+ * 任务保存入口，草稿箱，待一审等状态变更
+ * @author jiangjialin
+ *
+ */
 public interface MissionService {
 	
 	public Object saveMission(OperateDTO operateDTO);

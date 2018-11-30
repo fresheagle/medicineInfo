f96a83951f594ef84001a43ce3141cf5db131e18
DELETE <289385250@qq.com>
2018-11-30 16:18:28 +0800

    添加批量查询方法。

--- medicineInfo_dao/src/main/java/com/med/info/mapper/BaseMapper.java
+++ medicineInfo_dao/src/main/java/com/med/info/mapper/BaseMapper.java
@@ -3,6 +3,9 @@
  */
 package com.med.info.mapper;
 
+import java.awt.*;
+import java.util.List;
+
 /**
  * @author jialin.jiang
  *
@@ -13,5 +16,23 @@ public interface BaseMapper<T> {
 	public T selectByPrimaryKey(T t);
 	public int updateByPrimaryKey(T t);
 	public void updateByTaskIdSelective(T t);
-	
+
+	/**
+	 * 翻页查询所有数据
+	 * @return
+	 */
+	public List<T> showDataByPage();
+
+	/**
+	 * 查询所有结果数据
+	 * @return
+	 */
+	public List<T> showDataAll();
+
+	/**
+	 * 根据条件查询结果
+	 * @param t
+	 * @return
+	 */
+	public List<T> showDataCondition(T t);
 }

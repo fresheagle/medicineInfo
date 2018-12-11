e5961430519c8e19b3f864990450aa233a373e75
fresheagle <zm289385250@126.com>
2018-12-11 22:59:25 +0800

    新增用户和角色的绑定关系

--- medicineInfo_service/src/main/java/com/med/info/service/MissControlUserAndRoleService.java
+++ medicineInfo_service/src/main/java/com/med/info/service/MissControlUserAndRoleService.java
@@ -1,7 +1,9 @@
 package com.med.info.service;
 
 import com.med.info.domain.Miss_control_userAndRole;
+import com.med.info.mapper.domain.UserAndRoleDTO;
 
 public interface MissControlUserAndRoleService extends BaseService<Miss_control_userAndRole>{
 
+	public boolean insertUserAndRole(UserAndRoleDTO userAndRoleDTO);
 }

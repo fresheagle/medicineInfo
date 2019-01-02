6fec5c804abec081c1e192c082b270eac327f70b
fresheagle <zm289385250@126.com>
2019-01-02 23:24:56 +0800

    update审批信息和参考资料DAO和Service

---
+++ medicineInfo_service/src/main/java/com/med/info/service/MissControlApprovalService.java
@@ -0,0 +1,14 @@
+package com.med.info.service;
+
+import java.util.List;
+
+import com.med.info.domain.Miss_control_approvalWithBLOBs;
+
+public interface MissControlApprovalService extends BaseService<Miss_control_approvalWithBLOBs>{
+	/**
+	 * 根据taskId查询字段审批信息
+	 * @param taskId
+	 * @return
+	 */
+	List<Miss_control_approvalWithBLOBs> showByTaskId(String taskId);
+}
